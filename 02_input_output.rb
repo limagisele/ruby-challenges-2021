c740939718432fe258d8e654941847e49dc1187b
limagisele <limagisele05@gmail.com>
2022-03-14 17:47:29 +1100

    completed

--- 02_input_output.rb
+++ 02_input_output.rb
@@ -17,7 +17,7 @@
 # Beast mode challenge:
 # Ater the above, the program calculates how many years until
 # the user is 100 years old and tells the user. For example, for
-# our user input above (Tom and 40), after the program outputs 
+# our user input above (Tom and 40), after the program outputs
 # "Wow! You are 40 years old. Congratulations!"
 # The program also outputs:
 # "Tom, in 60 years you will be 100 years old!"
@@ -56,6 +56,31 @@
 # "Wow! You are 40 years old. Congratulations!"
 # "Tom, you are super old!"
 
-
 # Beast mode++ challenge:
 # Turn this program into a loop that will repeat these tasks until they type 'stop'
+
+loop do
+  puts "What's your name?"
+  name = gets.chomp.capitalize
+  puts "Hi #{name}! How old are you?"
+  age = gets.chomp.to_i
+  puts "Wow! You are #{age} years old. Congratulations!"
+
+  if age > 0 && age < 100
+    puts "#{name}, in #{100 - age} years you will be 100 years old!"
+  else
+    puts "#{name}, you are super old!"
+  end
+
+  puts 'Continue? (y/n)'
+  continue = gets.chomp.downcase
+
+  if continue == 'n'
+    break
+  elsif continue == 'y'
+    next
+  else
+    puts 'Invalid entry! Exiting program...'
+    break
+  end
+end
